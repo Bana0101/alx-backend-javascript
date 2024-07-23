f02bb7c9a1474b34335d997588c0621237c9e387
ayoub <sb.naba2@gmail.com>
2024-07-23 20:22:37 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,24 +2,20 @@ const http = require('http');
 
 const PORT = 1245;
 const HOST = 'localhost';
+const app = http.createServer();
 
-// Create HTTP server
-const app = http.createServer((req, res) => {
+
+app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
-  
-  // Send the response
-  res.write(responseText);
-  res.end(); // End the response
+  res.write(Buffer.from(responseText));
 });
 
-// Start the server
 app.listen(PORT, HOST, () => {
   process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
-
